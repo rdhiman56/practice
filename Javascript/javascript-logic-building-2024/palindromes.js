61df64e49a6ee59136c52c587b9d54e559095ca1
rdhiman56 <rdhiman56@gmail.com>
2024-10-05 19:56:09 +0530

    palindromes

--- Javascript/javascript-logic-building-2024/palindromes.js
+++ Javascript/javascript-logic-building-2024/palindromes.js
@@ -1,5 +1,22 @@
 // Check palindromes string
 
+// 2nd way
+function palindromes2(inputString) {
+    let start = 0;
+    let end = inputString.length-1;
+    for (start, end; start < end, start++, end--;) {
+      if (inputString[start] !== inputString[end]) {
+        return false
+      } 
+    }
+    return true;
+  }
+  
+  console.log('palindromes2=>', palindromes2("madam"));
+
+
+
+// 1st way
 function palindromes(inputString) {
   for (let i = 0; i <= inputString.length /2 ; i++) {
     if (inputString[i] !== inputString[inputString.length - 1 - i]) {
